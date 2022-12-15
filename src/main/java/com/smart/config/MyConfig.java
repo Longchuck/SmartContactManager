f02e3f9b5f9c89e58c03ed3c5e8ed3b4e488eddf
Longchuck <longtran.cmg@gmail.com>
2022-12-15 21:27:47 +0700

    Fist commit

---
+++ src/main/java/com/smart/config/MyConfig.java
@@ -0,0 +1,57 @@
+package com.smart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class MyConfig {
+
+	@Bean
+	public UserDetailsService getUserDetailService() {
+		return new UserDetailsServiceImpl();
+
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder() ;
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider() ;
+		daoAuthenticationProvider.setUserDetailsService(this.getUserDetailService());
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+		return daoAuthenticationProvider;
+	}
+
+	///config method..
+
+	@Bean
+	public AuthenticationManager authenticationManager(
+			AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.requestMatchers("/admin/**").hasRole("ADMIN")
+		.requestMatchers("/user/**").hasRole("USER")
+		.requestMatchers("/**").permitAll().and().formLogin().loginPage("/signin").and().csrf().disable();
+
+		http.formLogin().defaultSuccessUrl("/user/index", true);
+		return http.build();
+	}
+}
